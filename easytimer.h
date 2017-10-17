85071080b1764a6d36842316f2ea491f7144b8c0
miRoox <miroox@outlook.com>
2017-10-17 17:05:45 +0800

    refactor easytimer

--- easytimer.h
+++ easytimer.h
@@ -5,16 +5,21 @@
 #include <stdbool.h>
 #include <time.h>
 
+#define InvalidClockTime ((clock_t)(-1))
+
 struct _Timer;
 typedef struct _Timer Timer;
 
 extern Timer* installTimer(clock_t interval);
 extern void removeTimer(Timer* timer);
+extern void startTimer(Timer* timer);
+extern bool stopTimer(Timer* timer);
+extern void resetTimer(Timer* timer);
 extern bool enabledTimer(const Timer* timer);
+extern bool stoppedTimer(const Timer* timer);
 extern void setInterval(Timer* timer, clock_t interval);
 extern clock_t timerInterval(const Timer* timer);
 extern clock_t remainingTime(const Timer* timer);
-extern void resetTimer(Timer* timer);
 extern bool timeout(Timer* timer);
 
 #endif //EASY_TIMER_H
